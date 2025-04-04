02cbfc4334fb20df5b246b35f500f399a70ce216
Adam Stevenson <160768532+Adam-S988@users.noreply.github.com>
2025-04-04 10:03:41 -0230

    Toggle Previous Trees

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import BSTVisualizer from "./components/BSTVisualizer";
 import PreviousTrees from "./components/previous-trees";
 
 const App = () => {
-  const [currentTree, setCurrentTree] = useState(null);
+  const [treeData, setTreeData] = useState(null);
+  const [showPreviousTrees, setShowPreviousTrees] = useState(false);
+
+  const handleBSTCreated = (data) => {
+    setTreeData(data);
+  };
 
   return (
     <div>
       <h1>Binary Search Tree Visualizer</h1>
-      <NumberInput onBSTCreated={setCurrentTree} />
-      {currentTree && <BSTVisualizer treeJson={currentTree.treeJson} />}
-      <PreviousTrees />
+      <NumberInput onBSTCreated={handleBSTCreated} />
+      {treeData && <BSTVisualizer treeData={treeData} />}
+      <button onClick={() => setShowPreviousTrees(!showPreviousTrees)}>
+        {showPreviousTrees ? "Hide Previous Trees" : "Show Previous Trees"}
+      </button>
+
+      {showPreviousTrees && <PreviousTrees />}
     </div>
   );
 };
